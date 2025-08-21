4673af78fe0306f2eea4045ae93c96b93ca7b4f7
Edward F <43321188+EdwardAnalytics@users.noreply.github.com>
2025-08-21 08:55:26 +0100

    Updating dashboard information

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ def main():
                 This dashboard shows summary statistics and historic league tables for a selected mini-league.  
                  - **Data source:** All data comes from the _Previous Seasons_ section of each team’s _Gameweek History_, via the FPL API. 
                  - **Please note:** The dashboard only includes teams currently in the mini-league, and uses their past season records to build the historical view.  
-                 - **Customisation:** You can adjust the starting season year to choose how far back the history goes.  
+                 - **Settings:** You can adjust the starting season year to choose how far back the history goes.  
              """)
 
         # Input number
